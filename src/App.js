db0ff7758053470eccab390d586886a83047dd29
Andr-Shch <74563519+cybertrip-glitch@users.noreply.github.com>
2021-09-16 13:19:26 +0300

    witout pagination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useQuery } from "@apollo/client";
+import Card from "./components/Card/Card";
+import Loader from "./components/Loader/Loader";
+import getAllCharacters from "./querys";
+import './styles/main.style.css'
 
 
 function App() {
+ 
+  const {loading, error, data} = useQuery(getAllCharacters, {
+    variables: {page: 1}
+  })
+ 
+ if (loading) return <Loader/>
+ if (error) return <h1>Opss, something wrong</h1>
   return (
-    <div className="App">
+
+    <div className='wrapper'>
+    <header className="text-center">
+      <h1 className='text-center mt-3 '>The Rick and Morty</h1>
+      <h2 className='text-center mb-3'>multiverse</h2>
     
+    </header>
+    <div className="gri ">
+      {data?.characters?.results.map(character=>{
+        return <Card character={character} key={character.id}/>
+      })}
+    </div>
     </div>
   );
 }
